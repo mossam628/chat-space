f73b0f2cb69bdf3d4cc1c3dbef17ba924fa83d5d
mossam628 <mosososo628@gmail.com>
2020-02-19 20:29:54 +0900

    updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,7 +17,7 @@ $(function(){
             <p class="lower-message__content">
               ${message.content}
             </p>
-            ${message.image.url}
+            ${message.image}
           </div>
         </div>`
       } else if (message.content) {
@@ -54,7 +54,7 @@ $(function(){
             <p class="lower-message__content">
               // ${message.content}
             </p>
-            ${message.image.url}
+            ${message.image}
           </div>
         </div>`
       };
